5da93aac3dacb10efa537030b24bb973e100e0a9
Romain Delorme-Glorieux <romain.delorme-glorieux@signature-code.com>
2022-06-05 23:14:08 +0200

    Add StatusConverter

---
+++ MALBackup.App/StatusConverter.cs
@@ -0,0 +1,42 @@
+using MALBackup.Model;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MALBackup.App
+{
+    internal class StatusConverter : JsonConverter<Status>
+    {
+        internal static StatusConverter Instance => new();
+
+        public override Status Read( ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options )
+        {
+            if( reader.TokenType != JsonTokenType.String )
+            {
+                throw new InvalidDataException( "Value should be a string." );
+            }
+            string? status = reader.GetString();
+            return status switch
+            {
+                "watching" => Status.Watching,
+                "completed" => Status.Completed,
+                "on_hold" => Status.OnHold,
+                "dropped" => Status.Dropped,
+                "plan_to_watch" => Status.PlanToWatch,
+                _ => throw new InvalidDataException( $"Invalid status type '{status}'." )
+            };
+        }
+
+        public override void Write( Utf8JsonWriter writer, Status value, JsonSerializerOptions options )
+        {
+            writer.WriteStringValue( value switch
+            {
+                Status.Watching => "watching",
+                Status.Completed => "completed",
+                Status.OnHold => "on_hold",
+                Status.Dropped => "dropped",
+                Status.PlanToWatch => "plan_to_watch",
+                _ => throw new InvalidDataException( $"Unknown status type '{value}'." ),
+            } );
+        }
+    }
+}
